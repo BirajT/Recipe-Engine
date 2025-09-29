f7d98b56beab7bbd359dd8dbcb27040b77a2de23
BirajT <birajtamrakar803@gmail.com>
2025-09-29 20:58:11 +0545

    list added

---
+++ process.js
@@ -0,0 +1,11 @@
+//list
+//git branch ->list all available branches
+// git branch <branch_name> ->create new branch
+// git checkout <branch_name> ->switch branch
+// git checkout -b <new_branch_name> => create new branch +switch
+// git branch -d <branch_name> => delete branch locally
+// git pull <branch_name>
+//git merge <branch-name>
+
+git add .
+git commit -m "your message"
