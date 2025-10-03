f8b983d25f6cbda30b090ae5e46b3327182e3157
BirajT <birajtamrakar803@gmail.com>
2025-10-04 00:01:54 +0545

    Deleted unnesseary files

--- process.js
+++
@@ -1,9 +0,0 @@
-//git branch ->list all available branches
-// git branch <branch_name> ->create new branch
-// git checkout <branch_name> ->switch branch
-// git checkout -b <new_branch_name> => create new branch +switch
-// git branch -d <branch_name> => delete branch locally
-// git pull <branch_name>
-//git merge <branch-name>
-
-hjhg
